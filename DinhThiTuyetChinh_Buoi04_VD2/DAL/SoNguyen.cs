9278038ca144b0da4ff0e96e2e9191d62d046eb3
Tuyet Chinh <fengdinh@gmail.com>
2024-12-01 14:11:42 +0700

    VD2

---
+++ DinhThiTuyetChinh_Buoi04_VD2/DAL/SoNguyen.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class SoNguyen
+    {
+        public void timMaxMin(int A, int B, int C, out int max, out int min)
+        {
+            max = Math.Max(A, Math.Max(B, C));
+            min = Math.Min(A, Math.Min(B, C));
+        }
+    }
+}
---
+++ DinhThiTuyetChinh_Buoi04_VD2/DinhThiTuyetChinh_Buoi04_VD2/UnitTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DAL;
+namespace DinhThiTuyetChinh_Buoi04_VD2
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TC1_TimMaxMin()
+        {
+            SoNguyen dt = new SoNguyen();
+            int resultMax, resultMin;
+            dt.timMaxMin(3,7,2, out resultMax, out resultMin);
+
+            Assert.AreEqual(7, resultMax);
+            Assert.AreEqual(3, resultMin);
+        }
+
+        [TestMethod]
+        public void TC2_TimMaxMin()
+        {
+            SoNguyen dt = new SoNguyen();
+            int resultMax, resultMin;
+            dt.timMaxMin(3, 7, 2, out resultMax, out resultMin);
+
+            Assert.AreEqual(7, resultMax);
+            Assert.AreEqual(2, resultMin);
+        }
+    }
+}
